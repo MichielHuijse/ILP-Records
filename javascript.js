0019e0974083ec819a50f50e961d06fe1911a33b
Michiel <michiel.huijse@gmail.com>
2017-08-09 11:36:57 +0200

    responsive menu and ul gear responsiveness

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
+$( document ).ready(function() {
+	
+// Jquery script for sliding nav menu up and down when pressing on the hamburger.
+$( ".container" ).click(function() {
+  $( "ul#navlist" ).slideToggle( "slow" );});
+  
+ 
+});
 
+
+
+	
